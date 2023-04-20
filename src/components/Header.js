9da58270b6b74366bb03858c8963c9e14698910e
Anthony <129002769+Flikcer@users.noreply.github.com>
2023-04-20 18:08:57 -0400

    final working version updates server properly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = (props) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <Header1>{props.title}</Header1>
-      <Button
-        color={props.showAdd ? "#FFCDD2" : "lightblue"}
-        text={props.showAdd ? "Close" : "Add"}
-        onClick={props.onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={props.showAdd ? "#FFCDD2" : "lightblue"}
+          text={props.showAdd ? "Close" : "Add"}
+          onClick={props.onAdd}
+        />
+      )}
     </header>
   );
 };
